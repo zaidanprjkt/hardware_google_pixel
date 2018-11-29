63d16862e533cf22e09510611a10508c0cdd4aa0
Ethan Chen <intervigil@gmail.com>
2018-11-28 20:41:20 -0800

    pixel: Disable userdebug diagnostics
    * We can't make use of most of the debug features on userdebug builds.
    
    Change-Id: I31df2090b31243b742c4e82dc08cd8491dada54f

--- mm/device_gki.mk
+++ mm/device_gki.mk
@@ -9,7 +9,7 @@ PRODUCT_COPY_FILES += \
       hardware/google/pixel/mm/fstab.zram.50p:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.zram.50p \
       hardware/google/pixel/mm/fstab.zram.60p:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.zram.60p
 
-ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
 PRODUCT_PACKAGES += \
     mm_logd
 endif
